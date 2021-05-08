ec64c239ec834cc18905389d416b089ffa6c0ac6
Prantik <prantikroy7@gmail.com>
2021-05-08 13:04:19 +0530

    Updates

---
+++ precache-manifest.15270a29d8457a6986e114582f43feba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a490d4202ec636a6afd303838f9fce",
+    "url": "/ResumeBuilder/index.html"
+  },
+  {
+    "revision": "1673815edbdfcd00771c",
+    "url": "/ResumeBuilder/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "555f9a27e2f7e8d663a1",
+    "url": "/ResumeBuilder/static/css/main.c42f8367.chunk.css"
+  },
+  {
+    "revision": "1673815edbdfcd00771c",
+    "url": "/ResumeBuilder/static/js/2.bcbbef30.chunk.js"
+  },
+  {
+    "revision": "555f9a27e2f7e8d663a1",
+    "url": "/ResumeBuilder/static/js/main.2648edcd.chunk.js"
+  },
+  {
+    "revision": "9b3353abe368d92ea0b8",
+    "url": "/ResumeBuilder/static/js/runtime-main.1362d45b.js"
+  }
+]);
